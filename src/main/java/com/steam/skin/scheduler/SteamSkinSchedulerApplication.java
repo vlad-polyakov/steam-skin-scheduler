af9c233eb37160c2a0b7e5544367adf8f60409f7
vlad-polyakov <vladislavv.polyakov@mail.ru>
2026-07-09 01:15:24 +0300

    Remove unused imports

--- src/main/java/com/steam/skin/scheduler/SteamSkinSchedulerApplication.java
+++ src/main/java/com/steam/skin/scheduler/SteamSkinSchedulerApplication.java
@@ -1,14 +1,12 @@
 package com.steam.skin.scheduler;
 
-import com.steam.skin.scheduler.connector.SteamClientConnector;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SteamSkinSchedulerApplication {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(SteamSkinSchedulerApplication.class, args);
 	}
 
