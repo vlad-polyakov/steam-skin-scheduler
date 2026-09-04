08fb325a122b5fa377228d71f0ae2c38dbe1c884
vlad-polyakov <vladislavv.polyakov@mail.ru>
2026-09-04 13:50:07 +0300

    Add controller method. Little refactoring

--- src/main/java/com/steam/skin/scheduler/content/controller/ContentController.java
+++ src/main/java/com/steam/skin/scheduler/content/controller/ContentController.java
@@ -1,8 +1,10 @@
 package com.steam.skin.scheduler.content.controller;
 
 import com.steam.protobuf.ContentManifest;
+import com.steam.skin.scheduler.content.entity.vpk.VpkEntry;
 import com.steam.skin.scheduler.content.service.FileChunksService;
 import com.steam.skin.scheduler.content.service.ManifestService;
+import com.steam.skin.scheduler.content.util.vpk.VpkReader;
 import com.steam.skin.scheduler.getupdates.entity.pics.ContentInfo;
 import com.steam.skin.scheduler.getupdates.service.SteamVersionsCheckService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,17 +30,22 @@ public class ContentController {
     @Autowired
     private FileChunksService fileChunksService;
 
-    @GetMapping("/manifest")
-    public ResponseEntity<?> getManifest() throws Exception {
+    @GetMapping("/file/download")
+    public ResponseEntity<?> downloadFile(String filename) throws Exception {
         ContentInfo contentInfo = steamVersionsCheckService.getContentInfoForDownload();
         List<ContentManifest.ContentManifestPayload.FileMapping> manifestPayloadList = manifestService.downloadManifestPayload(contentInfo.getManifestId());
-        return ResponseEntity.ok(manifestPayloadList);
-    }
-
-
-
-    @GetMapping("/chunks")
-    public ResponseEntity<?> getChunks() throws Exception {
+        List<ContentManifest.ContentManifestPayload.FileMapping> manifestPak01DirList = manifestService.getFilesByDecryptedName(manifestPayloadList, "pak01_dir");
+        VpkEntry found;
+        for (ContentManifest.ContentManifestPayload.FileMapping fileMapping: manifestPak01DirList) {
+            byte[] data = fileChunksService.downloadChunk(fileMapping, DEPOT_ID);
+            List<VpkEntry> vpkEntry = VpkReader.readDirectory(data);
+            found = vpkEntry.stream().filter(vpk -> vpk.filename().contains(filename)).findFirst().orElse(null);
+            if(found != null) {
+                List<ContentManifest.ContentManifestPayload.FileMapping> manifestPak01IndexList = manifestService.getFilesByDecryptedName(manifestPayloadList, "pak01_" + found.archiveIndex());
+                byte[] fileData = fileChunksService.downloadRequiredOnlyChunks(manifestPak01IndexList.get(0), DEPOT_ID, found);
+                return  ResponseEntity.ok(fileData);
+            }
+        }
         return ResponseEntity.ok(null);
     }
 
